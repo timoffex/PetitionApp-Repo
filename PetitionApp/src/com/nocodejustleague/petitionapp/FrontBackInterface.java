48c9fa86dcaab93a9e6ab277b906071e8e6ad3fd
timoffex <timoffex@gmail.com>
2014-10-04 03:36:54 -0700

    Functions front-end should call to interact with back-end.

---
+++ PetitionApp/src/com/nocodejustleague/petitionapp/FrontBackInterface.java
@@ -0,0 +1,6 @@
+package com.nocodejustleague.petitionapp;
+
+public abstract class FrontBackInterface {
+	public abstract void vote(boolean like);
+	public abstract void vote(boolean like, String comment);
+}
